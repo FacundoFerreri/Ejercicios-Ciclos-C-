e4f2c06863d82b344cc5bdb15ce9a786055f24d5
FacundoFerreri <facundoferreri87@gmail.com>
2023-04-30 17:44:44 +0200

    Ciclo While

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ciclowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, con = 0;
+
+            Console.WriteLine("Ingrese un número: ");
+            n = int.Parse(Console.ReadLine());   
+            while (n != 0){
+                con++;
+                Console.WriteLine("Ingrese un número: ");
+                n = int.Parse(Console.ReadLine());                  
+        }
+              
+            Console.WriteLine("Ingresaste: " + con + " números");
+        }
+    }
+}
